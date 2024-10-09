da94eb0808b8b71111e377508cda2c45abe46bdf
Spider-Tree <122925491+Spider-Tree@users.noreply.github.com>
2024-10-09 11:27:47 -0600

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,4 +1,30 @@
 #bubble(list) -> sorted ist
+def bubble(item):
+    for i in range(len(item)-1):
+        skip=True
+        for j in range(len(item)-i-1):
+            a=item[j]
+            b=item[j+1]
+            if a>b:
+                item[j]=b
+                item[j+1]=a
+                skip=False
+        if (skip):
+           break
+    return item
+
+def interinsertionsort(unsorted,startI,gap):
+   
+    for i in range(gap+startI,len(unsorted),gap):
+        j=i
+        while j-gap>startI and unsorted[j-gap]>unsorted[j]:
+            unsorted[j-gap],unsorted[j]=unsorted[j],unsorted[j-gap]
+            j=j-gap
+        
+    return unsorted
+
+
+#selection(list) -> sorted list
 def selection(unsorted):
     for i in range(len(unsorted)-1):
         small = i
@@ -10,6 +36,7 @@ def selection(unsorted):
         unsorted[small] = temp
     return unsorted
 
+#insertion(list) -> sorted list
 def insertion(unsorted):
     for i in range(1,len(unsorted)):
         j=i
@@ -17,15 +44,7 @@ def insertion(unsorted):
             unsorted[j-1],unsorted[j]=unsorted[j],unsorted[j-1]
             j=j-1
     return unsorted
-def interinsertionsort(unsorted,startI,gap):
-   
-    for i in range(gap+startI,len(unsorted),gap):
-        j=i
-        while j-gap>startI and unsorted[j-gap]>unsorted[j]:
-            unsorted[j-gap],unsorted[j]=unsorted[j],unsorted[j-gap]
-            j=j-gap
-        
-    return unsorted
+#shell(list) -> sorted list
 def shell(unsorted,gapValues):
     
     for gap in gapValues:
@@ -48,6 +67,7 @@ def party(unsorted,low,high):
               low=low+1
               high=high-1
     return high
+#quicksort(list) -> sorted list
 def Quicksort(unsorted,low,high):
     if(low>=high):
         return 
@@ -55,23 +75,3 @@ def Quicksort(unsorted,low,high):
     quicksort(unsorted,low,lowEnd)
     quicksort(unsorted,lowEnd+1,high)
     return unsorted
-#selection(list) -> sorted list
-
-#insertion(list) -> sorted list
-
-#shell(list) -> sorted list
-
-#quicksort(list) -> sorted list
-def bubble(item):
-    for i in range(len(item)-1):
-        skip=True
-        for j in range(len(item)-i-1):
-            a=item[j]
-            b=item[j+1]
-            if a>b:
-                item[j]=b
-                item[j+1]=a
-                skip=False
-        if (skip):
-           break
-    return item
